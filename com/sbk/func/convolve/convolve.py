499392aa7c8098796097b8d89f882eefa5e8f1ab
SergiySobolev <sergiy.sobolev@gmail.com>
2018-05-16 14:00:31 +0300

    [Chapter 2] convolve_machine + tests

--- com/sbk/func/convolve/convolve.py
+++ com/sbk/func/convolve/convolve.py
@@ -10,3 +10,18 @@ def convolve(signal, impulse_response):
         for j in range(0, impulse_response_length):
             result[i + j] += signal[i] * impulse_response[j]
     return result
+
+
+def convolve_machine(signal, impulse_response):
+    signal_length = len(signal)
+    impulse_response_length = len(impulse_response)
+    result_length = signal_length + impulse_response_length - 1
+    result = np.zeros(result_length)
+    for i in range(0, result_length):
+        for j in range(0, impulse_response_length):
+            cur_signal_index = i - j
+            if cur_signal_index < 0 or cur_signal_index >= signal_length:
+                continue
+            result[i] += signal[i-j] * impulse_response[j]
+    return result
+
--- tests/com/sbk/func/convolve/test_convolve_machine.py
+++ tests/com/sbk/func/convolve/test_convolve_machine.py
@@ -1,57 +1,55 @@
 import unittest
 
-import numpy.testing as npt
 import numpy as np
+import numpy.testing as npt
 
-from com.sbk.func.convolve.convolve import convolve
+from com.sbk.func.convolve.convolve import convolve_machine
 
 
-class TestConvolve(unittest.TestCase):
+class TestConvolveMachine(unittest.TestCase):
 
     def setUp(self):
         self.x = range(0, 1000)
         self.sin = np.sin(self.x)
-        #self.lg = np.log(self.x)
-        #self.signal = self.sin + self.lg
+        # self.lg = np.log(self.x)
+        # self.signal = self.sin + self.lg
 
     def test_convolve_compare_with_numpy(self):
         signal = [0, -1, -1.2, 2, 1.4, 1.4, 0.8, 0, -0.8]
         impulse_response = [1, 0.5, 0.2, 0]
-        res = convolve(signal, impulse_response)
-        expected_res = [0.0, -1.0, -1.7,  1.2,  2.16,  2.5,   1.78,  0.68, -0.64, -0.4, -0.16,  0.0]
+        res = convolve_machine(signal, impulse_response)
+        expected_res = [0.0, -1.0, -1.7, 1.2, 2.16, 2.5, 1.78, 0.68, -0.64, -0.4, -0.16, 0.0]
         numpy_res = np.convolve(signal, impulse_response)
-        npt.assert_array_equal(res, numpy_res)
+        npt.assert_array_almost_equal(res, numpy_res)
         npt.assert_array_almost_equal(res, expected_res)
 
     def test_convolve_with_delta_function(self):
         signal = [1.0, 2.0, 3.0, 4.0, 5.0]
         impulse_response = [1, 0, 0]
         npt.assert_array_equal(signal,
-                               np.trim_zeros(convolve(signal, impulse_response), trim="b"))
+                               np.trim_zeros(convolve_machine(signal, impulse_response), trim="b"))
 
     def test_convolve_with_amplifier_function(self):
         signal = [1.0, 2.0, 3.0, 4.0, 5.0]
         impulse_response = [2, 0, 0]
         res = [2.0, 4.0, 6.0, 8.0, 10.0]
         npt.assert_array_equal(res,
-                               np.trim_zeros(convolve(signal, impulse_response), trim="b"))
+                               np.trim_zeros(convolve_machine(signal, impulse_response), trim="b"))
 
     def test_convolve_with_shift_function(self):
         signal = [1.0, 2.0, 3.0, 4.0, 5.0]
         impulse_response = [0, 0, 1]
         res = [0, 0, 1.0, 2.0, 3.0, 4.0, 5.0]
-        npt.assert_array_equal(res, convolve(signal, impulse_response))
+        npt.assert_array_equal(res, convolve_machine(signal, impulse_response))
 
     def test_convolve_with_shift_and_amply_function(self):
         signal = [1.0, 2.0, 3.0, 4.0, 5.0]
         impulse_response = [0, 0, 3]
         res = [0, 0, 3.0, 6.0, 9.0, 12.0, 15.0]
-        npt.assert_array_equal(res, convolve(signal, impulse_response))
+        npt.assert_array_equal(res, convolve_machine(signal, impulse_response))
 
     def test_convolve_with_echo_function(self):
         signal = [1.0, 2.0, 3.0, 4.0, 5.0]
         impulse_response = [1, 0, 0, 0.5]
         res = [1.0, 2.0, 3.0, 4.5, 6.0, 1.5, 2.0, 2.5]
-        npt.assert_array_equal(res, convolve(signal, impulse_response))
-
-
+        npt.assert_array_equal(res, convolve_machine(signal, impulse_response))
